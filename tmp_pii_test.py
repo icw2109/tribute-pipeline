a7d9ee9a606b2da3cdb6804e2af7f2c4829552ef
Iain Wagner <icw2109@columbia.edu>
2025-09-30 08:22:54 -0500

    \Remove temporary PII test file"

--- tmp_pii_test.py
+++
@@ -1,5 +0,0 @@
-from insights.classifier_pipeline import ClassifierPipeline, PipelineConfig
-pipe=ClassifierPipeline(PipelineConfig(enable_self_train=False, enable_zero_shot=False, debug=True))
-rec=pipe.classify_text('Contact us at founders@example.com or +1 415-555-1234 for enterprise support growth rate 20%.')
-print(rec['text'])
-print('piiScrubbed', rec['debug']['piiScrubbed'])
